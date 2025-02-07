c16f59c78f1186652aac6918ce173426e0552cda
Pratik Kumar <pratikk12093@gmail.com>
2025-02-07 18:09:42 +0530

    create bcrypt logic

--- auth.js
+++ auth.js
@@ -1,5 +1,4 @@
 //set up passport with a local authentication Strategy, using model for use 
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Person = require('./models/Person')
@@ -7,12 +6,12 @@ const Person = require('./models/Person')
 passport.use(new LocalStrategy( async (USERNAME,password,done)=>{
     //authentication logic here
     try {
-        console.log('Received credentials : ',USERNAME,password);
+        //nsole.log('Received credentials : ',USERNAME,password);
         const user =await Person.findOne({username : USERNAME });
         if (!user) {
             return done(null, false, { message: 'Incorrect username.' });
         }
-        const isPasswordMatch = user.password === password ? true : false;
+        const isPasswordMatch = user.comparePassword(password);
         if(isPasswordMatch){
             return done(null,user);
         }
--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { deleteOne } = require('./MenuItems');
 //Define the person Schema/so that what you want from user in what schema so that input on point to point
 const personSchema = new mongoose.Schema({
     name: {
@@ -38,6 +40,39 @@ const personSchema = new mongoose.Schema({
         required:true
     }
 })
+
+//pre is middleware function executes before save();
+personSchema.pre('save',async function (next) {
+    const person = this;//sara data person me store hoga..
+
+    //Hash the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();//Returns true if any of the given paths are modified, else false. If no arguments, returns true if any path in this document is modified.
+    try{
+        //hash password generation : 
+        const salt = await bcrypt.genSalt(10);//10 digit ka salt generator..
+        
+        //hash Password : ye hashed password banata h hash + password leke
+        const hashedPassword = await bcrypt.hash(person.password,salt);
+        person.password = hashedPassword;//assign the user password with hashed password
+
+        next();
+    }
+    catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try {
+         //Use bcrypt to compare the providec password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+
+    } catch (error) {
+       throw error;
+    }
+}
+
 //Create Person Model
 const Person = mongoose.model('Person',personSchema);
 module.exports = Person;
